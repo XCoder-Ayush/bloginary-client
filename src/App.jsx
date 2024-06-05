21b7a406719a3ac46bb8253e835dfc4b59d47985
Amrikghosh20000 <amrikghosh15@gmail.com>
2024-06-06 00:19:49 +0530

    Navbar Component Created need to check the toggle functionality for responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
 
   return (
     <>
--- src/components/Navbar/Navbar.js
+++
@@ -1,8 +0,0 @@
-function Navbar(){
-    return(
-        <>
-        <h1>Dream 11</h1>
-        </>
-    )
-}
-export default Navbar;
