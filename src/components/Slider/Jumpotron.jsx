694547fa5ee1ad1c95b3a7f2de072f29aafcf684
Amrikghosh20000 <amrikghosh15@gmail.com>
2024-06-06 18:53:12 +0530

    Small change in Slider Component

--- src/components/Slider/Jumpotron.jsx
+++ src/components/Slider/Jumpotron.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 function Jumpotron() {
   return (
     <>
-      <section className="bg-center bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply')] bg-gray-700 bg-blend-multiply">
+      <section className="bg-center bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply">
         <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
           <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
             We welcome you to the world of blog's
