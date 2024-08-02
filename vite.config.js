0d35362f1470b36b5de38320ea4ec1cff5a157d0
Ayush Sharma <arp23359@gmail.com>
2024-08-03 02:56:27 +0530

    [feat] : Updated vite.config for prod access.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/bloginary-client',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
