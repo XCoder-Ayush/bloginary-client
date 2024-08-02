53fb3dbfaa8f8c53c6571fad052e9ea545390f2a
Ayush Sharma <arp23359@gmail.com>
2024-08-03 01:55:35 +0530

    [feat] : Github Action Workflow With Permissions Updated.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/bloginary-client/',
+  // base: '/bloginary-client/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
