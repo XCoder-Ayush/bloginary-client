15481e14e5301097719835ff90a3054b8ff975d1
Ayush Sharma <arp23359@gmail.com>
2024-08-29 00:52:30 +0530

    fix(test): changed build project directory.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -8,5 +9,8 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  build: {
+    outDir: path.resolve(__dirname, 'bloginary-client/dist'), // Correct placement of build config
   },
 });
